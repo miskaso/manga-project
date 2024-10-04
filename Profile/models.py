04872dc86c88c904f79c0adb33c23b8e50a6eb87
miskaso <qwertyclop@gmail.com>
2024-10-04 19:57:11 +0600

    new

---
+++ Profile/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    name = models.ForeignKey(User, on_delete=models.CASCADE())
+    bio = models.TextField()
+    avatar = models.ImageField()
+    fon = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+
+class Notification(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE())
+    manga_id = models.ForeignKey(Manga, on_delete=models.CASCADE())
+    description = models.CharField(max_length=100, default='Вышла новая глава!')
