477473e935f53c7196a63c4b5379acbd47392783
miskaso <qwertyclop@gmail.com>
2024-10-18 18:57:21 +0600

    ыуфкср

--- Manga/serializers.py
+++ Manga/serializers.py
@@ -7,3 +7,26 @@ class MangaSerializer(serializers.ModelSerializer):
         model = Manga
         fields = '__all__'
 
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+class SearchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Manga
+        exclude = ['img', 'chapters', 'description']
--- Manga/views.py
+++ Manga/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from .models import Author, Manga, Category, Tag
-from .serializers import MangaSerializer
+from .serializers import MangaSerializer, SearchSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import BasePermission
-
-
+from django.db.models import Q
 
 # Create your views here.
 
@@ -22,5 +21,33 @@ class ShowManga(viewsets.ModelViewSet):
 
     permission_classes = [IsAdminOrRead]
 
+
 class SearchView(viewsets.ModelViewSet):
-    queryset = Manga.objects.filter()
+    serializer_class = SearchSerializer
+    queryset = Manga.objects.all()
+
+    def get_queryset(self):
+        manga = self.request.query_params.get('title')
+        author = self.request.query_params.get('author')
+        year = self.request.query_params.get('year')
+        tags = self.request.query_params.get('tags')
+        category = self.request.query_params.get('category')
+
+        filters = Q()
+
+        if manga:
+            filters &= Q(title__icontains=manga)
+        if author:
+            filters &= Q(author__icontains=author)
+        if year:
+            filters &= Q(year__year=year)
+        if tags:
+            filters &= Q(tags__icontains=tags)
+        if category:
+            filters &= Q(category__icontains=category)
+
+        # Если фильтры не заданы, возвращаем все результаты
+        if filters:
+            queryset = queryset.filter(filters)
+
+        return queryset
