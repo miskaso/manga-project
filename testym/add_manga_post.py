629f836c391452032234bd35bdf3ce89d1c4fde9
miskaso <qwertyclop@gmail.com>
2024-11-01 19:58:14 +0600

    addmangappost

--- testym/add_manga_post.py
+++ testym/add_manga_post.py
@@ -6,16 +6,16 @@ url = 'http://127.0.0.1:8000/manga/show/'
 path = '1520984778_site-portfolio_6.jpg'
 
 
-with open(path, 'rb') as a:
+with open(path, 'rb') as a, open(path,'rb') as f:
     date = {
         'title': 'myaso',
         'description': 'jhfklasd jfjdaslf ads',
         'year': '2001-02-25',
     }
 
-    files = {'chapters': a, 'img': a}
+    files = {'chapters': a, 'img': f}
 
-    response = requests.post(url=url, data=date, headers=post_login.headers,files=files)
+    response = requests.post(url=url, data=date, headers=post_login.headers, files=files)
 
 if response.status_code == 201:
     print('Успешно', response.text)
